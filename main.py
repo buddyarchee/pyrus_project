95f50d8a26a95b29934a518eeb2f945d254ac2f6
buddyarchee <artua1510@gmail.com>
2024-10-07 21:48:40 +0300

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from docx import Document
+
+def get_text_from_docx(path):
+    doc = Document(path)
+    full_text = []
+    for paragraph in doc.paragraphs:
+        full_text.append(paragraph.text)
+    return {
+        'statusCode': 200,
+        'body': '\n'.join(full_text),
+    }
+'''asdsd'''
+print(get_text_from_docx(r'C:\folder\esotericism.docx'))
